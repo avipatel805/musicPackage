124c47ffad6ef4ee680ca0654b95a48e5feeea56
avipatel805 <avipatel805@gmail.com>
2025-08-12 23:30:30 -0500

    Initial Commit

---
+++ Music Package/Music_PackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PackageApp.swift
+//  Music Package
+//
+//  Created by Avi Patel on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Music_PackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
